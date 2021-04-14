ae4c20044ee8b1dc08679f918eccd350afc92c32
Adwait Kelkar <67591857+adwaitkelkar@users.noreply.github.com>
2021-04-14 12:06:01 +0530

    1st
    1st commit

---
+++ js/script.js
@@ -0,0 +1,96 @@
+
+
+$(document).ready(function(){
+
+    // wow initiation
+    new WOW().init();
+
+    // navigation bar toggle
+    $('#navbar-toggler').click(function(){
+        $('.navbar-collapse').slideToggle(400);
+    });
+
+    // navbar bg change on scroll
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+        if(pos >= 100){
+            $('.navbar').addClass('cng-navbar');
+        } else {
+            $('.navbar').removeClass('cng-navbar');
+        }
+    });
+
+    
+    
+
+    // team carousel 
+    $('.team .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 1
+            }, 
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            }
+        }
+    });
+
+    // faq accordion
+    $('.counter-count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+          
+          //chnage count up speed here
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+
+    // testimonial carousel 
+    $('.testimonial .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        items: 1
+    });
+
+    //pricing
+    $(document).ready(function($) {
+        //Check if an element was in a screen
+        function isScrolledIntoView(elem){
+            var docViewTop = $(window).scrollTop();
+            var docViewBottom = docViewTop + $(window).height();
+            var elemTop = $(elem).offset().top;
+            var elemBottom = elemTop + $(elem).height();
+            return ((elemBottom <= docViewBottom));
+        }
+        //Count up code
+   
+        //Start animation on page-load
+        if ( isScrolledIntoView(".counter") ) {
+            countUp();
+        }
+        //Start animation on screen
+        $(document).scroll(function() {
+            if ( isScrolledIntoView(".counter") ) {
+                countUp();
+            }
+        });
+    });
+
+});
